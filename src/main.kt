47b0fc301a2961be3b6c9db6c432f23066b3fbf1
Mundurujoana <mundujoana@gmail.com>
2022-04-13 11:40:25 +0300

    commit strings

---
+++ src/main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    var president = "Yoweri Museveni"
+    var oldvalue = "yoweri"
+    var newvalue = "joana"
+
+
+    println(president.startsWith("oldvalue"))
+    println(president.endsWith("newvalue"))
+}
